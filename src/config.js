8725feb1192748e3171b98187aa4859a2f9efc41
somoneFromHell <somone_from_hell@tutanota.com>
2023-08-17 09:34:16 +0530

    starting project

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "1046023766296-qs0cm8fjsp19dostpgbunefopsm00u72.apps.googleusercontent.com",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+  api: {
+    API_URL: "https://api-node.themesbrand.website",
+  }
+};
